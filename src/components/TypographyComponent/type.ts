02640450a9eaf04250c154905e56a4d1398564a7
BeoDVB <127584129+tinwinvn@users.noreply.github.com>
2025-05-25 20:28:38 +0700

    Home Page UI

---
+++ src/components/TypographyComponent/type.ts
@@ -0,0 +1,6 @@
+type TVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'p';
+
+type TSize = 'small' | 'medium' | 'large';
+
+
+export type { TVariant, TSize };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,274 @@
-import { DarkModeToggler } from "../components/DarkModeToggler"
+import Typography from "../components/TypographyComponent"
 
 export const HomePage = () => {
   return (
-    <DarkModeToggler />
+    <div>
+      <div className="flex-col items-center px-[50px] bg-white dark:text-white dark:bg-[#0f0f11] justify-between">
+        <div className="h-[412px] w-full flex-col">
+          <div className="pt-6">
+            <Typography variant="h4" size="large" >Truyện Vừa Ra Mắt</Typography>
+          </div>
+          <div className="mt-7 h-80 border border-black rounded-[10px] bg-[#1c1c1f] flex">
+            <div className="h-full min-w-1/5 bg-[#d9d9d9] rounded-[10px]" />
+            <div className="my-5 mx-6 flex-col">
+              <Typography variant="h3" size="large">Osoraku Kanojo wa Ore no Aniki wo Neratteru Osoraku Kanojo wa Ore no Aniki wo Neratteru Kanojo wa Ore no...</Typography>
+              <div className="flex gap-2.5">
+                <div className="border-2 rounded-[5px] w-fit px-1.5 py-0.5 bg-black">Trinh Thám</div>
+                <div className="border-2 rounded-[5px] w-fit px-1.5 py-0.5 bg-black">Hài</div>
+                <div className="border-2 rounded-[5px] w-fit px-1.5 py-0.5 bg-black">Lãng mạn</div>
+                <div className="border-2 rounded-[5px] w-fit px-1.5 py-0.5 bg-black">Học đường</div>
+                <div className="border-2 rounded-[5px] w-fit px-1.5 py-0.5 bg-black">Gia đình</div>
+              </div>
+              <div className="mt-4">
+                <Typography variant="p" size="small" className="font-sans">Năm 2095, khi thế giới bước vào kỷ nguyên sống song song giữa thực và ảo, công nghệ DataLink cho phép con người đồng bộ ý thức trực tiếp vào không gian dữ liệu. Ở đó, mọi thứ đều được mã hóa thành bit và byte, tạo ra một thế giới giả lập sống động hơn cả thực tại. Zero, một hacker vô danh, tỉnh dậy bên trong DataVerse mà không nhớ mình là ai và vì sao bị “đóng gói” thành một chuỗi dữ liệu. Với sự giúp sức của V, một AI quản lý hệ thống, Zero phát hiện ra rằng toàn bộ thế giới số đang dần bị thao túng bởi tổ chức bí ẩn mang tên BlackGate.</Typography>
+              </div>
+              <div className="py-3.5 flex justify-between">
+                <div className="italic text-lg">
+                  Iris Cavana
+                </div>
+                <div className="flex gap-14">
+                  <div className="text-[#ff6740]">
+                    NO.1
+                  </div>
+                  <div>
+                    P
+                  </div>
+                   <div>
+                    N
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="my-8 h-[595px] w-full border border-black bg-[#1c1c1f] gap-5 flex rounded-[10px]">
+          <div className="w-1/3">
+            <div className="h-14 bg-[#585876] rounded-[10px] flex items-center justify-center">
+              <Typography>ĐỌC NHIỀU NHẤT</Typography>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="h-14 bg-[#585876] rounded-[10px] flex items-center justify-center">
+              <Typography>XU HƯỚNG MỚI</Typography>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="h-14 bg-[#585876] rounded-[10px] flex items-center justify-center">
+              <Typography>ĐÁNH GIÁ CAO</Typography>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+            <div className="h-[88px] mt-[15px] px-5 py-1 flex">
+              <div className="bg-[#d9d9d9] h-[80px] min-w-[60px] rounded-[10px]" />
+              <div className="mx-2.5 mt-3">
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+                <div className="text-[13px] py-[1px]">
+                  Atticus’s Odyssey: Reincar...
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
