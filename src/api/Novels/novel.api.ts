41356bca8482d3e0dce1ebe13fdb977c0372d868
nguyennhuloc2003 <locnhunguyen2003@gmail.com>
2025-07-03 13:36:12 +0700

    Merge branch 'develop' into feature/loc/novelRead

--- src/api/Novels/novel.api.ts
+++ src/api/Novels/novel.api.ts
@@ -1,7 +1,6 @@
 import http from "../../utils/http";
 import type { Novels } from "./novel.type";
 
-export const GetNovels = () => http.get<Novels>('Novels');
 interface GetNovelsParams {
   page?: number
   limit?: number
