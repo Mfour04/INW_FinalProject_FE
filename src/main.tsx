552609eeb5d0c4aafd306c3e78f971dc812dc89e
nguyennhuloc2003 <locnhunguyen2003@gmail.com>
2025-07-13 10:05:34 +0700

    [Feature][FE] Fix conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from './context/ThemeContext/ThemeContext.tsx'
-import { AuthProvider } from './context/AuthContext/AuthProvider.tsx'
-import { ToastProvider } from './context/ToastContext/toast.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./context/ThemeContext/ThemeContext.tsx";
+import { AuthProvider } from "./context/AuthContext/AuthProvider.tsx";
+import { ToastProvider } from "./context/ToastContext/toast.tsx";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <ToastProvider>
-        <AuthProvider>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider>
-              <App />
-            </ThemeProvider>
-          </QueryClientProvider>
-        </AuthProvider>
-      </ToastProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <ToastProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </AuthProvider>
+    </ToastProvider>
+  </BrowserRouter>
+);
